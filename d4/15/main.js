fb57cdbc2be636ac2d49997d818b30e1893c25fb
inji0212 <in17100170@kakao.com>
2024-07-19 09:29:41 +0900

    d4: 15_문자 내장 객체

--- d4/15/main.js
+++ d4/15/main.js
@@ -69,3 +69,19 @@ console.log(
   } in the sentence`
 );
 // Expected output: "The word "fox" is in the sentence"
+
+//bind()
+const module = {
+  x: 42,
+  getX: function () {
+    return this.x;
+  },
+};
+
+const unboundGetX = module.getX;
+console.log(unboundGetX()); // The function gets invoked at the global scope
+// Expected output: undefined
+
+const boundGetX = unboundGetX.bind(module);
+console.log(boundGetX());
+// Expected output: 42
