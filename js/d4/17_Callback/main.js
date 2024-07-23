1c79df9fef30b48528d8cc1f503e161817ac7d90
inji0212 <in17100170@kakao.com>
2024-07-23 10:09:51 +0900

    D4: JS 17 Callback

--- js/d4/17_Callback/main.js
+++ js/d4/17_Callback/main.js
@@ -122,7 +122,8 @@ task5(() => {
 // 콜백이 안됨
 
 //이러한 콜백의 단점을 보완-> promise
-
+//resolve가 필요
+//함수의 내부에서 resolve()
 function task10() {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -169,3 +170,94 @@ task10() //
 // task13 시작
 // task14 시작
 // 모든작업 끝
+
+//async await방식
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const task15 = async () => {
+  await delay(1000);
+  console.log("task15시작");
+  return "task15완료";
+};
+
+async function task16() {
+  console.log("task16 시작");
+  return;
+}
+async function task17() {
+  console.log("task17 시작");
+  return;
+}
+async function task18() {
+  return console.log("task18 시작");
+}
+const task19 = async () => console.log("task19 시작");
+
+task15() //
+  .then(() => task16())
+  .then(() => task17())
+  .then(() => task18())
+  .then(() => task19());
+
+const task20 = async () => {
+  await delay(1000);
+  return "task20t시작";
+};
+const task21 = async () => "task21 시작";
+const task22 = async () => "task22 시작";
+const task23 = async () => "task23 시작";
+const task24 = async () => "task24 시작";
+
+const startTasks = async () => {
+  const msg1 = await task20();
+  const msg2 = await task21();
+  const msg3 = await task22();
+  const msg4 = await task23();
+  const msg5 = await task24();
+  console.log(msg1, msg2, msg3, msg4, msg5);
+};
+
+startTasks();
+
+const task25 = async () => {
+  await delay(1000);
+  return "task25시작";
+};
+const task26 = async () => {
+  await delay(2000);
+  return "task26시작";
+};
+const task27 = async () => {
+  await delay(1000);
+  return "task27시작";
+};
+
+const startTasks2 = async () => {
+  const msg1 = await task25();
+  const msg2 = await task26();
+  const msg3 = await task27();
+
+  console.log(msg1, msg2, msg3);
+};
+
+startTasks2(); //4초
+
+// 각 걸린시간들의 합이됨-> 직렬
+
+const startTasks3 = async () => {
+  const task1promise = task25();
+  const task2promise = task26();
+  const task3promise = task27();
+  // await이 아니라서 병렬 처리
+
+  const msg1 = await task1promise;
+  const msg2 = await task2promise;
+  const msg3 = await task3promise;
+
+  console.log(msg1, msg2, msg3);
+};
+
+startTasks3(); //2초
+
+//혹은 promise.all
